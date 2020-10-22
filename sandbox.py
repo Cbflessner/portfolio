458e021f35c130dad4fe9f88b70732412bd60c41
Christian <cbflessner@gmail.com>
2020-10-22 12:14:13 -0600

    tweaking test_read_config()

--- sandbox.py
+++ sandbox.py
@@ -1,28 +1,34 @@
 #############################test_create_kafka_topics()################################
+# import tests.test_kafka_utils as ku
+# from confluent_kafka import Producer
+
+# kafka = ku.TestKafkaUtils
+# kafka.test_create_kafka_topic(kafka)
+
+# conf = {'bootstrap.servers':'broker:9092'}
+# p = Producer(conf)
+# info = p.list_topics()
+# topic = info.topics['christian_test'].topic
+# print(topic)
+
+# #############################test_producer()################################
+# import tests.test_messages as tm
+
+# messageController = tm.TestMessages
+# result = messageController.test_producer(messageController)
+# print(result)
+
+
+# #############################test_consumer()################################
+# import tests.test_messages as tm
+
+# messageController = tm.TestMessages
+# result = messageController.test_consumer(messageController)
+# print(result)
+
+
+###########################test_read_config()##############################
 import tests.test_kafka_utils as ku
-from confluent_kafka import Producer
 
 kafka = ku.TestKafkaUtils
-kafka.test_create_kafka_topic(kafka)
-
-conf = {'bootstrap.servers':'localhost:9092'}
-p = Producer(conf)
-info = p.list_topics()
-topic = info.topics['christian_test'].topic
-print(topic)
-
-#############################test_producer()################################
-import tests.test_messages as tm
-
-messageController = tm.TestMessages
-result = messageController.test_producer(messageController)
-print(result)
-
-
-#############################test_consumer()################################
-import tests.test_messages as tm
-
-messageController = tm.TestMessages
-result = messageController.test_consumer(messageController)
-print(result)
-
+kafka.test_read_config(kafka)
