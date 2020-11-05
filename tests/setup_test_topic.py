55fbc2bdd02a83015217041572b221c35774f3fc
Christian <cbflessner@gmail.com>
2020-11-05 16:10:41 -0700

    changing name of the config in the setup_test_topic script

--- tests/setup_test_topic.py
+++ tests/setup_test_topic.py
@@ -12,7 +12,7 @@ from confluent_kafka import Producer
 import time
 
 #configure and create the producer we will use to check the status of our new topic 
-conf = kafka_utils.read_config(portfolio_path+'/kafka/librdkafka.config')
+conf = kafka_utils.read_config(portfolio_path+'/kafka/kafka.config')
 producer_config = {'bootstrap.servers': conf['bootstrap.servers']}
 kafka_utils.create_topic(conf=producer_config, topic='christian_test', num_partitions=1, replication_factor=1) 
 p = Producer(producer_config)
